6a0b1ac4557e1b71fddebf8681708c02271dce38
vikasmotla <vikas.motla@gmail.com>
2018-11-22 09:53:25 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,130 +1,136 @@
 var canvas = document.getElementById('myCanvas')
 var c = canvas.getContext("2d");
-var w = window.innerWidth - 20;
-var h = window.innerHeight - 20;
+var w = window.innerWidth-20;
+var h = window.innerHeight-20;
+var alpha = 0.5;
+var alpha1 = 0.5;
 
-// var letters =
-var color = ['lightgreen', 'red', 'white', 'cyan']
 canvas.width = w;
 canvas.height = h;
 
-var dotsArray = [];
-var dotsArray1 = [];
-
 
 var mouse = {
-  mouseX: w / 2,
-  mouseY: h / 2
-}
-
-var index = 0
-
-
-setInterval(function () {
-  if (dotsArray.length<100) {
-    index++
-    dotsArray.push(new MainFun(w/2, h-20, color[Math.floor(Math.random()*color.length)] , 2, index))
-  }
-}, 25);
-
-for (var i = 0; i < 0; i++) {
-
-  // var randX = Math.random()
-  // randX = Math.floor(randX*2)==1 ? -randX : randX
-  // var randY = Math.random()
-  // randY = Math.floor(randX*2)==1 ? -randY : randY
-  // var x = randX*w;
-
-  dotsArray[i] = new MainFun(w/2, h-20, color[Math.floor(Math.random()*color.length)] , 2)
+  mouseX:w/2,
+  mouseY:h/2
 }
 
 
-function MainFun(x, y, col, rad, indx) {
-  this.indx = indx
+document.addEventListener('mousemove',function (event) {
+
+  mouse.mouseX = event.clientX;
+  mouse.mouseY = event.clientY;
+
+})
+
+var dotsArray = [];
+var dotsArray2 = [];
+
+
+
+function MainFun(x,y,col,rad) {
   this.x = x;
   this.y = y;
   this.col = col;
   this.rad = rad;
-  this.dy = Math.floor(Math.random() * 3.5) + 2
+  this.alpha = 0.1;
+  // this.aplpha1 = 0.5;
 
-  this.dy1 = this.dy / 2
-  this.dx = 0.3
-  this.bounce = false
-  this.gAlpha = 0.05
+  this.dx = -0.5
+  this.dy = -0.1
 
-  this.dx1 = 0.5
-
-  this.maxRange = this.dy * 80
-  this.minRange = Math.floor(Math.random() * 15) + 20
-  if (Math.random() < 0.5) {
-    this.dx = -this.dx
-    this.dx1 = -this.dx1
-  }else{
-    this.dx = this.dx
-    this.dx1 = this.dx1
-  }
-
-  this.update = function() {
-
-    if (this.y < this.maxRange) {
-      this.dy1 = -this.dy1
-      this.dy = -this.dy
-    }
-
-    if (Math.abs(this.y - this.maxRange) <20) {
-      this.y -=this.dy1
-      this.x -=this.dx1
-    }else {
-      this.y -= this.dy
-      this.x -= this.dx
-    }
-
-    if (this.dy<0) {
-          if (this.y>h) {
-            this.dy = -this.dy
-            this.bounce = true
-          }
-    }
-
-    if (this.bounce) {
-      if (h- this.y > this.minRange) {
-        this.col = 'black'
-        console.log(this.dy);
-      }
-    }
+  this.update = function () {
+    this.x += this.dx;
+    this.y += this.dy;
 
     this.draw()
   }
 
-  this.draw = function() {
+  this.draw = function () {
+
+    if ( Math.abs(this.x - mouse.mouseX) < 20 ) {
+    this.alpha = 1
+      // this.col = 'blue'
+    }else {
+        this.alpha = 0.1
+      // this.col = 'white'
+    }
+
     c.beginPath();
-    c.arc(this.x, this.y, this.rad, 0, 2 * Math.PI);
+    c.arc(this.x,this.y,this.rad,0,2*Math.PI);
+    // c.lineWidth=2;
+    // c.strokeStyle = this.col;
+    // c.stroke();
     c.fillStyle = this.col;
+    c.globalAlpha = this.alpha;
     c.fill();
     c.closePath();
 
+    for (var i = 0; i < dotsArray.length; i++) {
+      // dotsArray[i].
 
-    c.beginPath();
-    c.moveTo(w/2-10,h);
-    c.lineTo(w/2,h-25);
-    c.lineTo(w/2+10,h);
-    c.closePath();
+      // if ( Math.abs(dotsArray[i].x - mouse.mouseX) < 20 ) {
+      //   this.alpha = 1
+      //   console.log('inside if');
+      //   // this.col = 'blue'
+      // }else {
+      //   this.alpha = 0.1
+      // }
 
-    // the outline
-    c.stroke();
+      c.beginPath();
+      c.moveTo(dotsArray[i].x,dotsArray[i].y);
+      c.lineTo(dotsArray2[i].x,dotsArray2[i].y);
+      // console.log(alpha);
 
-    // the fill color
-    c.fillStyle = "cyan";
-    c.fill();
+      c.lineWidth=0.04;
+      c.strokeStyle = this.col;
+      c.globalAlpha = this.alpha;
+      c.closePath();
+      c.stroke();
+    }
+
   }
 
 }
 
+var x0 = w/2
+var y0 = h/2 + 200;
+
+var x1 = w/2 + 100;
+var y1 = h/2 - 200;
+
+
+var yVar = 15;
+
+for (var i = 0; i < 20; i++) {
+   x0 = x0+20
+   if (yVar>0) {
+     y0 = y0+yVar
+     yVar = -yVar
+   }else {
+     y0 = y0+yVar
+     yVar = -yVar
+   }
+  dotsArray[i] = new MainFun( x0 , y0,'#ffffff', 3 )
+}
+
+for (var i = 0; i < 20; i++) {
+   x1 = x1+20
+   if (yVar>0) {
+     y1 = y1+yVar
+     yVar = -yVar
+   }else {
+     y1 = y1+yVar
+     yVar = -yVar
+   }
+  dotsArray2[i] = new MainFun( x1 , y1,'#ffffff', 3 )
+}
+
 function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, w, h)
-  for (var i = 0; i < dotsArray.length; i++) {
-    dotsArray[i].update()
-  }
+ requestAnimationFrame(animate)
+ c.clearRect(0,0,w,h)
+ for (var i = 0; i < dotsArray.length; i++) {
+   dotsArray[i].update()
+   dotsArray2[i].update()
+ }
 }
 animate()
